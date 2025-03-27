86ee76dd417e322d08c2dc251324684a08ae553f
chalermsak-s <kaidohjung@gmail.com>
2025-03-27 17:16:15 +0700

    Add

--- sanbox_testing/server/src/repository/academicPositionRepository.ts
+++ sanbox_testing/server/src/repository/academicPositionRepository.ts
@@ -1,12 +1,12 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 export function getAllAcademicPositions() {
-  return prisma.academic_position.findMany()
+  return prisma.academic_position.findMany();
 }
 
 export function getAcademicPositionById(id: number) {
   return prisma.academic_position.findUnique({
-    where: { id },
-  })
+    where: { id }
+  });
 }
